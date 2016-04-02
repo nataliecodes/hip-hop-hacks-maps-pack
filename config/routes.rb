44dba044ebdf1322f0e57d7291f84be887536283
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-04-02 18:03:25 -0400

    Add searches create, index routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :maps
     resources :welcome, only: [:index]
+    resources :searches, only: [:index, :create]
 
   # Example resource route with options:
   #   resources :products do
