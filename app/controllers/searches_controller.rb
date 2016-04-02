6faad91369d66c6b6e22808146008bfe20cc883e
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-04-02 18:50:44 -0400

    Include songable in searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,14 @@
 class SearchesController < ApplicationController
+	include Songable
+	
 	def index
 		@search_results = "Hello"
 	end
 
 	def create
-		song_query = params[:"song-query"]
+		query = params[:"song-query"]
 		#song_list = genius_query_list(song_query) James scrape method
-		
+
+		binding.pry
 	end
 end
