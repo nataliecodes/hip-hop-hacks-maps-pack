fd767f66c997a4fde5a4a57656e7da3c47e1e488
Pete <peternatewood@gmail.com>
2016-04-02 17:11:08 -0400

    Remove .ready wrapper from map.js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,9 +1,7 @@
-$(document).ready(function() {
-  window.map;
-  window.initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 0
-    });
-  }
-});
+var map;
+var initMap = function() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 4
+  });
+}
