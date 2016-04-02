c9387dbec41bdf7d833fc7e2770d42e39230da37
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-04-02 18:04:35 -0400

    Add preliminary search controller logic

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,11 @@
 class SearchesController < ApplicationController
 	def index
+		@search_results = "Hello"
+	end
+
+	def create
+		song_query = params[:"song-query"]
+		#song_list = genius_query_list(song_query) James scrape method
 		
 	end
 end
