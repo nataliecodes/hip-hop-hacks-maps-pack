46f1bec7d420c05868166e2cf20f820a7c69776d
Pete <peternatewood@gmail.com>
2016-04-02 16:55:48 -0400

    Add api key constant in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  API_KEY = ENV["GOOGLE_MAPS_API_KEY"]
 end
