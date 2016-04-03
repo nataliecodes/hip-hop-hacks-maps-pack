397373b96b4f8241e503a7b5c29d67c7a4828679
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-04-02 22:01:29 -0400

    Save query results as class variable and render index again

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
 	include Songable
-	
+
 	def index
 		@search_results = "Hello"
 	end
@@ -8,7 +8,8 @@ class SearchesController < ApplicationController
 	def create
 		query = params[:"song-query"]
 		#song_list = genius_query_list(song_query) James scrape method
+		@query_results = list_query_results(query)
 
-		binding.pry
+		render 'index'
 	end
 end
