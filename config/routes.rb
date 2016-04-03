251833f55334d03f96d0621734561744b1318ae6
nataliecodes <nataliepolen@gmail.com>
2016-04-03 14:30:22 -0400

    Add custom route to add geocode search to maps controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
       get 'maps' => 'maps#index'
   post 'geocode_search' => 'maps#geocode_search'
-
+  post 'create_query' => 'maps#create_query'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
