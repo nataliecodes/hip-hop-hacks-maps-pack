7e37d2c874897470e9dc6ecab1e84e698dc372fe
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-04-02 22:02:36 -0400

    Make list_query_results method functional, and require open-uri

--- lib/songable.rb
+++ lib/songable.rb
@@ -1,6 +1,12 @@
 module Songable
+	require 'open-uri'
   def list_query_results(query)
   	puts "These are query results"
+  	query.gsub!(' ', '-')
+  	doc = Nokogiri::HTML(open("http://genius.com/search?q=#{query}"))
+
+		#I think this has same effect as #each_with_object([]) James?
+  	choices = doc.css('li.search_result').to_a 
   end
 
 end
