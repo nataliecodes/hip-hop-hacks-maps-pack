66f69d4f6372cae2c9f8348002974b23eb561d9d
Pete <peternatewood@gmail.com>
2016-04-03 12:41:23 -0400

    Update maps.js to only create a new map if one doesn't exist

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,15 +1,10 @@
-var map
+var map;
 window.markerPositions = new Array;
 var initMap = function() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -30, lng: 150},
-    zoom: 4
-  });
-  markerPositions.forEach(function(position) {
-    new google.maps.Marker({
-      map: map,
-      position: position,
-      title: "Test Marker"
+  if(!map) {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -30, lng: 150},
+      zoom: 8
     });
-  });
+  }
 }
