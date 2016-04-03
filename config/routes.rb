e00f076b920dc19c0545ea64ea4726e3649a3065
Pete <peternatewood@gmail.com>
2016-04-03 11:46:10 -0400

    Add maps#geocode_search in config

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
       get 'maps' => 'maps#index'
+  post 'geocode_search' => 'maps#geocode_search'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
