3224b8e4dbaf5c3ffc4f40ca701f161b8cece0ab
Pete <peternatewood@gmail.com>
2016-04-03 11:46:27 -0400

    Add maps#geocode_search in controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,15 @@
 class MapsController < ApplicationController
-  GEOCODE_URI = "https://maps.googleapis.com/maps/api/geocode/"
+  GEOCODE_URI = "https://maps.googleapis.com/maps/api/geocode/json"
 
 	def index
 		
 	end
+
+  def geocode_search
+    response = HTTParty.get(GEOCODE_URI, {address: params[:query], key: ENV["GOOGLE_MAPS_API_KEY"]})
+    @marker_positions = response["results"].map do |result|
+      return result["geometry"]["location"]
+    end
+    render json: {html: render_to_string("maps/_map", layout: false, locals: {api_key: API_KEY})}
+  end
 end
