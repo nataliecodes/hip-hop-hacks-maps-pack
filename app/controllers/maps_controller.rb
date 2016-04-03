1959c2ff78dbbed5ca01c408579b5a290a97158f
Pete <peternatewood@gmail.com>
2016-04-03 11:53:58 -0400

    Update maps#geocode_search to include marker_positions in response

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,9 +7,12 @@ class MapsController < ApplicationController
 
   def geocode_search
     response = HTTParty.get(GEOCODE_URI, {address: params[:query], key: ENV["GOOGLE_MAPS_API_KEY"]})
-    @marker_positions = response["results"].map do |result|
+    marker_positions = response["results"].map do |result|
       return result["geometry"]["location"]
     end
-    render json: {html: render_to_string("maps/_map", layout: false, locals: {api_key: API_KEY})}
+    render json: {
+      html: render_to_string("maps/_map", layout: false, locals: {api_key: API_KEY}),
+      marker_positions: marker_positions
+    }
   end
 end
