0ca7f564526aed86a2dae26045532fbe9781b3d2
Darius <darius.atmar@gmail.com>
2016-04-03 21:55:27 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :maps, only: [:index, :create]
+    resources :maps
   # Example resource route with options:
   #   resources :products do
   #     member do
