fc612a9843ca1e53b216b979dc82abafab237274
nataliecodes <nataliepolen@gmail.com>
2016-04-08 11:42:58 -0400

    Change map location to central US when opened on homepage

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,8 +3,8 @@ window.markerPositions = new Array;
 var initMap = function() {
   if(!map) {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -30, lng: 150},
-      zoom: 8
+      center: {lat: 40.817, lng: -102.173},
+      zoom: 4
     });
   }
 
