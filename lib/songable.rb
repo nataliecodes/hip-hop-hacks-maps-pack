0d5e73c6ea6f142e939b6b5a16f368cc8716de86
nataliecodes <nataliepolen@gmail.com>
2016-04-08 11:53:31 -0400

    Remove puts statement from Songable

--- lib/songable.rb
+++ lib/songable.rb
@@ -1,7 +1,6 @@
 module Songable
   require 'open-uri'
   def list_query_results(query)
-    puts "These are query results"
     query.gsub!(' ', '-')
     doc = Nokogiri::HTML(open("http://genius.com/search?q=#{query}"))
     doc.css('li.search_result').map do |result|
